96de7e7f84b55f685c9ff2be36a27dabbecfe81a
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2026-01-04 21:57:54 -0300

    Initial Commit

---
+++ Voice-Noise-Reductor/Voice_Noise_ReductorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Voice_Noise_ReductorApp.swift
+//  Voice-Noise-Reductor
+//
+//  Created by Isaque da Silva on 1/4/26.
+//
+
+import SwiftUI
+
+@main
+struct Voice_Noise_ReductorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
