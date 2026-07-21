fd4697cd561ee43e4658eaa0356c58490c2a489f
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2026-07-21 15:46:18 -0300

    Initial Commit

---
+++ Voice-Memos/Voice_MemosApp.swift
@@ -0,0 +1,17 @@
+//
+//  Voice_MemosApp.swift
+//  Voice-Memos
+//
+//  Created by Isaque da Silva on 7/21/26.
+//
+
+import SwiftUI
+
+@main
+struct Voice_MemosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
